0d9a979629d3b46dc75fdb745fe2ae5f4b19a9ac
misko02 <misko20023@wp.pl>
2025-10-23 08:43:30 +0200

    CT-74- Fix misspell I made during solving merge conflict

--- client/src/services/fetchAllAutoCorrelations.ts
+++ client/src/services/fetchAllAutoCorrelations.ts
@@ -18,7 +18,7 @@ export async function fetchAllAutoCorrelations(
   for (const category of Object.keys(filenamesPerCategory)) {
     for (const filename of filenamesPerCategory[category]) {
       try {
-        const autocorrelation = await fetchAutoCorrelation(category, filename);
+        const autocorrelation = await fetchAutocorrelation(category, filename);
         if (!autocorrelationsValues[category]) {
           autocorrelationsValues[category] = {};
         }
